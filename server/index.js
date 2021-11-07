1c52b05202efe674172f356b5d1e2999414a3346
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2021-11-07 16:23:07 +0500

    third chapter , graphql server side for crude completed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,53 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
+// const { graphqlHTTP } = require('express-graphql');
+// const { buildSchema } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express()
+// resolvers
+const { portfolioQueries, portfolioMutations } = require('./graphql/resolvers');
+// types
+const { portfolioTypes } = require('./graphql/types');
+
+app.prepare().then( async () => {
+  const server = express();
+
+    // Construct a schema, using GRAPHQL schema language
+  const typeDefs = gql(`
+    ${portfolioTypes}
+
+    type Query {
+      hello: String
+      portfolio(id: ID): Portfolio
+      portfolios: [Portfolio]
+    }
+
+    type Mutation {
+      createPortfolio(input: PortfolioInput): Portfolio
+      updatePortfolio(id: ID , input : PortfolioInput ): Portfolio
+      deletePortfolio(id : ID): ID
+    }
+  `);
+
+// The root provides a resolver for each API endpoint
+  const resolvers = {
+    Query: {
+      ...portfolioQueries
+    },
+    Mutation: {
+      ...portfolioMutations
+    }
+  }
+
+  const apolloServer = new ApolloServer({typeDefs, resolvers})
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app: server})
+
 
   server.all('*', (req, res) => {
     return handle(req, res)
