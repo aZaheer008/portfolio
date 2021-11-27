e0071b1ffcfacf1c60c3e79f61d333f02a006e21
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2021-11-27 18:04:36 +0500

    messages display completed

--- pages/login.js
+++ pages/login.js
@@ -3,11 +3,14 @@ import withApollo from '@/hoc/withApollo';
 import { useSignIn } from '@/apollo/actions';
 import Redirect from '@/components/shared/Redirect';
 import BaseLayout from '@/layouts/BaseLayout';
+import { useRouter } from 'next/router';
+import messages from '@/variables/messages';
 
 const Login = () => {
 
     const [ signIn ,{data,loading, error}] = useSignIn();
-
+    const router = useRouter();
+    const { message } = router.query;
     const errorMessage = (error) => {
         console.log("--------error----",error);
         return (error.graphQLErrors && error.graphQLErrors[0].message) || 'Ooops something went wrong...';
@@ -19,6 +22,7 @@ const Login = () => {
                 <div className="row">
                 <div className="col-md-5 mx-auto">
                     <h1 className="page-title">Login</h1>
+                    { message && <div className={`alert alert-${messages[message].status}`}>{messages[message].value}</div>}
                     <LoginForm loading={loading} onSubmit={
                         (signInData) => signIn({variables:signInData})
                     }/>
